431ab1da738ceb74e9d5ea1cc4f230dc9e3883a6
davidmdm <desmarais.michaud.david@gmail.com>
2021-11-13 21:29:37 -0500

    added cors middleware

--- middleware.go
+++ middleware.go
@@ -2,7 +2,9 @@ package muxter
 
 import (
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 // Middleware is a function that takes a handler and modifies its behaviour by returning a new handler
@@ -47,6 +49,76 @@ func Recover(recoverHandler func(recovered interface{}, rw http.ResponseWriter, 
 					return
 				}
 			}()
+			h.ServeHTTP(rw, r)
+		})
+	}
+}
+
+// AccessControlOptions provides options for the CORS middleware.
+type AccessControlOptions struct {
+	// AllowOrigin is the origin that is set for the Access-Control-Allow-Origin. If it is "*" and
+	// AllowCredentials is true the incoming Origin will be used.
+	AllowOrigin string
+
+	// AllowOriginFunc takes the request origin and returns the Access-Control-Allow-Origin.
+	// Takes precedence over AllowOrigin.
+	AllowOriginFunc func(origin string) string
+
+	// MaxAge sets the Access-Control-Max-Age property.
+	MaxAge time.Duration
+
+	AllowCredentials bool
+	ExposeHeaders    []string
+	AllowHeaders     []string
+	AllowMethods     []string
+}
+
+func CORS(opts AccessControlOptions) Middleware {
+	if opts.AllowOrigin == "" {
+		opts.AllowOrigin = "*"
+	}
+	allowOrigin := opts.AllowOrigin
+
+	if opts.AllowMethods == nil {
+		opts.AllowMethods = []string{"GET", "POST", "HEAD", "PUT", "PATCH", "DELETE"}
+	}
+	allowMethods := strings.Join(opts.AllowMethods, ", ")
+	allowHeaders := strings.Join(opts.AllowHeaders, ", ")
+
+	return func(h http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+			if opts.AllowOriginFunc == nil && allowOrigin == "*" && opts.AllowCredentials {
+				rw.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+				rw.Header().Add("Vary", "Origin")
+			} else if opts.AllowOriginFunc != nil {
+				rw.Header().Set("Access-Control-Allow-Origin", opts.AllowOriginFunc(r.Header.Get("Origin")))
+				rw.Header().Add("Vary", "Origin") // Let browsers know that Access-Control-Allow-Origin varies by Origin
+			} else {
+				rw.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+			}
+
+			if opts.MaxAge > 0 {
+				rw.Header().Set("Access-Control-Max-Age", strconv.Itoa(int(opts.MaxAge.Seconds())))
+			}
+
+			if opts.AllowCredentials {
+				rw.Header().Set("Access-Control-Allow-Credentials", "true")
+			}
+
+			if r.Method == "OPTIONS" {
+				if allowHeaders != "" {
+					rw.Header().Set("Access-Control-Allow-Headers", allowHeaders)
+				} else {
+					rw.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+					rw.Header().Add("Vary", "Access-Control-Request-Headers")
+				}
+
+				rw.Header().Set("Access-Control-Allow-Methods", allowMethods)
+
+				rw.WriteHeader(204)
+				return
+			}
+
 			h.ServeHTTP(rw, r)
 		})
 	}

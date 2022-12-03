dfd6881fc402f1fef2e2942ba1cc9cc64b485d30
davidmdm <desmarais.michaud.david@gmail.com>
2022-12-03 12:28:00 -0500

    added more realistic multi registed route benchmarks

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -148,3 +148,85 @@ func BenchmarkRoutingParamsNestedMuxes(b *testing.B) {
 		root.ServeHTTP(w, r)
 	}
 }
+
+func BenchmarkRealistic(b *testing.B) {
+	libs := map[string]func(routes []string) http.Handler{
+		"muxter": func(routes []string) http.Handler {
+			mux := New()
+			for _, route := range routes {
+				mux.HandleFunc(route, func(w http.ResponseWriter, r *http.Request, c Context) {})
+			}
+			return mux
+		},
+		"httprouter": func(routes []string) http.Handler {
+			router := httprouter.New()
+			for _, route := range routes {
+				router.GET(route, func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {})
+			}
+			return router
+		},
+		"gin": func(routes []string) http.Handler {
+			gin.SetMode(gin.ReleaseMode)
+			engine := gin.New()
+			for _, route := range routes {
+				engine.GET(route, func(ctx *gin.Context) {})
+			}
+			return engine
+		},
+		"echo": func(routes []string) http.Handler {
+			router := echo.New()
+			for _, route := range routes {
+				router.GET(route, func(c echo.Context) error { return nil })
+			}
+			return router
+		},
+	}
+
+	cases := []struct {
+		Name   string
+		Routes []string
+		Path   string
+	}{
+		{
+			Name: "no params",
+			Routes: []string{
+				"/public/assets",
+				"/app",
+				"/api/v1/resource",
+				"/api/v2/result",
+				"/api/v2/results",
+			},
+			Path: "/api/v2/results",
+		},
+		{
+			Name: "with params",
+			Routes: []string{
+				"/public/assets",
+				"/app",
+				"/api/:version",
+				"/api/:version/resource",
+				"/api/:version/result",
+				"/api/:version/results/:id",
+			},
+			Path: "/api/v3/results/23",
+		},
+	}
+
+	for lib, genHandler := range libs {
+		b.Run(lib, func(b *testing.B) {
+			for _, tc := range cases {
+				b.Run(tc.Name, func(b *testing.B) {
+					h := genHandler(tc.Routes)
+					r := httptest.NewRequest("GET", tc.Path, nil)
+					w := httptest.NewRecorder()
+
+					b.ResetTimer()
+
+					for i := 0; i < b.N; i++ {
+						h.ServeHTTP(w, r)
+					}
+				})
+			}
+		})
+	}
+}
